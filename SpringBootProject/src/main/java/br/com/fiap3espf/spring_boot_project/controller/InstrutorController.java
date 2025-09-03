f9fcefed96805bcd7d2553fdd54a115c47db4c71
GeovannaSCunha <geovanna.scunha@gmail.com>
2025-09-03 09:33:12 -0300

    AULA 03 - Listagem

--- SpringBootProject/src/main/java/br/com/fiap3espf/spring_boot_project/controller/InstrutorController.java
+++ SpringBootProject/src/main/java/br/com/fiap3espf/spring_boot_project/controller/InstrutorController.java
@@ -1,14 +1,15 @@
 package br.com.fiap3espf.spring_boot_project.controller;
 
 import br.com.fiap3espf.spring_boot_project.instrutor.DadosCadastroInstrutor;
+import br.com.fiap3espf.spring_boot_project.instrutor.DadosListagemInstrutor;
 import br.com.fiap3espf.spring_boot_project.instrutor.Instrutor;
 import br.com.fiap3espf.spring_boot_project.instrutor.InstrutorRepository;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/instrutor")
@@ -20,8 +21,14 @@ public class InstrutorController {
 
     @PostMapping
     @Transactional
-    public void cadastrarInstrutor(@RequestBody DadosCadastroInstrutor dados) {
+    public void cadastrarInstrutor(@RequestBody @Valid DadosCadastroInstrutor dados) {
         //System.out.println(dados);
         repository.save(new Instrutor(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemInstrutor> listarInstrutores() {
+        return repository.findAll().stream().
+                map(DadosListagemInstrutor::new).toList();
     }
 }
